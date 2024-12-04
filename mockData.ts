c33429c45f913880aeccb07a6d4e71ef750634f2
caliitru <linz.stephenson@gmail.com>
2024-12-04 15:35:06 -0500

    Create mockData.ts

---
+++ mockData.ts
@@ -0,0 +1,9 @@
+import { MockData } from './types';
+
+export const mockData: MockData[] = [
+  { month: 'Aug', score: 490 },
+  { month: 'Sep', score: 525 },
+  { month: 'Oct', score: 608 },
+  { month: 'Nov', score: 645 },
+  { month: 'Dec', score: 672 }
+];
