da53ce87245004091a96e7263b98d276fb0975b0
caliitru <linz.stephenson@gmail.com>
2024-12-04 22:27:51 -0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,12 @@
+// Barrel exports file
+export { default as Dashboard } from './DashboardPreview';
+export { ScoreCircle } from './ScoreCircle';
+export { StatsCards } from './StatsCards';
+export { ScoreChart } from './ScoreChart';
+export { AIInsights } from './AIInsights';
+
+// Export types
+export type * from './types';
+
+// Export mock data if needed for testing/development
+export { mockData } from './mockData';
