654e8f59d5eec21313a1e6fb2894607e7b2441c8
caliitru <linz.stephenson@gmail.com>
2024-12-05 20:19:50 -0500

    Update mockData.ts

--- src/components/shared/Dashboard/mockData.ts
+++ src/components/shared/Dashboard/mockData.ts
@@ -1,7 +1,7 @@
 import { MockData } from './types';
 
 export const mockData: MockData[] = [
-  { month: 'Aug', score: 490 },
+  { month: 'Aug', score: 502 },
   { month: 'Sep', score: 525 },
   { month: 'Oct', score: 608 },
   { month: 'Nov', score: 645 },
